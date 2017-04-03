667a5e774f86c3ee4f894149cb9c6e74f6ef1056
Mark DiFranco <markdifranco@car-booth-10-mac.local>
2017-04-03 10:19:39 -0700

    Initial Commit

---
+++ Speedometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speedometer
+//
+//  Created by Mark DiFranco on 4/3/17.
+//  Copyright © 2017 Mark DiFranco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
